67c8648b6d41588177a259e7886ac25960acb13d
Marcin Kufel <marcinkufel84@gmail.com>
2020-07-31 11:21:20 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+{
+
+    const tasks = [
+        {
+            content: "pouczyć się",
+            done:false,
+        },
+        {
+            content: "poćwiczyć żeby schudnąć",
+            done: true,
+        },
+    ]
+
+    const render = () => {
+        let htmlString = "";
+
+        for(const task of tasks) {
+            htmlString += `
+            <li>
+            ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+
+    };
+
+    init();
+
+}
