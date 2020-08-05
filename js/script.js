67601daaac978741a2450fe566416df95ce387a8
Marcin Kufel <marcinkufel84@gmail.com>
2020-08-05 10:26:48 +0200

    binding events moved to a separate function

--- js/script.js
+++ js/script.js
@@ -24,12 +24,40 @@
         render();
     }
 
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    };
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+
+        });
+
+
+    
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+        });
+
+    });
+    }
+
     const render = () => {
         let htmlString = "";
 
         for(const task of tasks) {
             htmlString += `
             <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+            <button class="js-done">zrobione?</button>
             ${task.content}
 
             <button class="js-remove">usuń</button>
@@ -39,15 +67,8 @@
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
-        const removeButtons = document.querySelectorAll(".js-remove");
-
-        removeButtons.forEach((removeButton, index) => {
-            removeButton.addEventListener("click", () => {
-                removeTask(index);
-            });
-
-        });
-    };
+        bindEvents();
+};
 
 
 
